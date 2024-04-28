d4c4121a5cd0c43384b1b12131463e1228417e63
naeem4633 <ahmedn3700@gmail.com>
2024-04-28 22:28:03 +0500

    added a div, changed main div to absolute to fix its position

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -10,7 +10,6 @@ export default function Main(props) {
     var typed = new Typed('#element', {
       strings: ['React',"Node Js","HTML"," CSS","Github","Python","React Native","Adobe Illustrator", "Ui/Ux"],
       typeSpeed: 50,
-      loop:true
     });
 
     return () => {
@@ -25,28 +24,33 @@ export default function Main(props) {
         className="flex justify-center items-center lg:justify-around lg:h-screen h-screen lg:flex-row mt-10"
       >
         <div className="lg:w-1/2 flex justify-center items-center">
-          <div className="text-left text-gray-100">
-            <p className="text-lg lg:text-4xl">
-              Hello, my name is <span className="text-purple-500">Ali.</span>
-              <br /> I'm a  Web Developer
-              <br /> Currently working in <span id="element" className="text-purple-500"> React</span>
-            </p>
-            <div className="flex space-x-4 pt-4">
-              <a
-                href={ResumeFile}
-                download="Ali_Resume.pdf"
-                className="bg-teal-800 hover:scale-1 ease-in-out duration-500  transition-all w-38 text-sm hover:bg-teal-500 text-white px-4 py-2 rounded"
-              >
-                Download Resume
-              </a>
-              <a
-                href="https://github.com/Aliarshad0014"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-teal-800 hover:scale-1 ease-in-out duration-500  transition-all w-18 text-sm hover:bg-teal-500 text-white px-4 py-2 rounded"
-              >
-                Github
-              </a>
+          <div className="w-full relative text-left text-gray-100 flex flex-col justify-center">
+            <div className="w-full absolute text-lg lg:text-4xl flex flex-col items-center">
+              <div className="w-2/3">
+                <div className="w-full">
+                  <p>Hello, my name is <span className="text-purple-500">Ali.</span>
+                  <br /> I'm a  Web Developer
+                  <br /> Currently working in
+                  <br /> <span id="element" className="text-purple-500"> React</span></p>
+                </div>
+                <div className="flex space-x-4 pt-4">
+                  <a
+                    href={ResumeFile}
+                    download="Ali_Resume.pdf"
+                    className="bg-teal-800 hover:scale-1 ease-in-out duration-500  transition-all w-38 text-sm hover:bg-teal-500 text-white px-4 py-2 rounded"
+                  >
+                    Download Resume
+                  </a>
+                  <a
+                    href="https://github.com/Aliarshad0014"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-teal-800 hover:scale-1 ease-in-out duration-500  transition-all w-18 text-sm hover:bg-teal-500 text-white px-4 py-2 rounded"
+                  >
+                    Github
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
